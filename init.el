262f03ed96f09a2ba84e3cc48ca7627c6e62d16d
michaellaufer <michael.laufer@toganetworks.com>
2022-03-11 10:39:04 +0200

    updates

--- init.el
+++ init.el
@@ -9,111 +9,17 @@
   (package-refresh-contents)
   (mapc #'package-install package-selected-packages))
 
-;; Bootstrap John Wigley's "use-package"
+;; use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
 
-;; remove UI 
+;; remove UI
+(setq inhibit-startup-screen t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-
-;; Zenburn
-(load-theme 'zenburn t)
-
-;; Power Line
-(require 'powerline)
-(powerline-default-theme)
-
-;; Iedit
-(require 'iedit)
-
-;; Ido
-(ido-mode 1)
-(ido-everywhere 1)
-
-;; Ido Completing Read Plus
-(require 'ido-completing-read+)
-(ido-ubiquitous-mode 1)
-
-;; Ivy
-(ivy-mode)
-(counsel-mode)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(setq search-default-mode #'char-fold-to-regexp)
-;; (global-set-key "\C-s" 'swiper)
-
-
-;; Company
-(add-hook 'after-init-hook 'global-company-mode)
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 1)
-(setq company-show-numbers t)
-
-;; Which Key
-(which-key-mode)
-
-;; YASnippet
-(yas-global-mode 1)
-
-(defvar my-company-point nil)
-(advice-add 'company-complete-common
-            :before (lambda () (setq my-company-point (point))))
-(advice-add 'company-complete-common
-            :after (lambda ()
-  		     (when (equal my-company-point (point))
-  		       (yas-expand))))
-
-;; LSP
-(add-hook 'c-mode-hook 'lsp)
-(add-hook 'c++-mode-hook 'lsp)
-(require 'lsp-python-ms)
-(setq lsp-python-ms-auto-install-server t)
-(add-hook 'python-mode-hook #'lsp)
-(use-package lsp-python-ms
-  :ensure t
-  :init (setq lsp-python-ms-auto-install-server t)
-  :hook (python-mode . (lambda ()
-                         (require 'lsp-python-ms)
-                         (lsp))))
-(add-hook 'latex-mode-hook 'lsp)
-(add-hook 'markdown-mode-hook 'lsp)
-(setq lsp-clients-clangd-args
-      '("--header-insertion=never"
-        "--header-insertion-decorators=0"))
-(with-eval-after-load 'lsp-mode
-  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
-  (require 'dap-cpptools)
-  (yas-global-mode))
-
-;; Flycheck
-(add-hook 'c-mode-hook #'flycheck-mode)
-(add-hook 'c++-mode-hook #'flycheck-mode)
-(add-hook 'python-mode-hook #'flycheck-mode)
-
-;; Smartparens
-(require 'smartparens-config)
-(add-hook 'c-mode-hook #'smartparens-mode)
-(add-hook 'c++-mode-hook #'smartparens-mode)
-(add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
-(add-hook 'python-mode-hook #'smartparens-mode)
-
-;; When you press RET, the curly braces automatically add another newline.
-(sp-with-modes '(asm-mode c-mode c++-mode emacs-lisp-mode  python-mode)
-  (sp-local-pair "{" nil :post-handlers '(("||\n[i]" "RET")))
-  (sp-local-pair "/*" "*/" :post-handlers '(("| " "SPC") ("* ||\n[i]" "RET"))))
-(setq sp-escape-quotes-after-insert nil)
-
-
-;; Winner
-(when (fboundp 'winner-mode)
-  (winner-mode 1))
-
-;; Alarm Bell
-(setq ring-bell-function 'ignore)
 
 ;; Line Number Mode
 (if (version<= "26.0.50" emacs-version)
@@ -123,22 +29,78 @@
 ;; Column Number Mode
 (column-number-mode 1)
 
+;; Alarm Bell
+(setq ring-bell-function 'ignore)
+
+
 ;; Place auto-save files into system's temporary file
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-;; Org Mode
-(eval-after-load "org"
-  '(require 'ox-md nil t))
+;; y for yes
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; kill current buffer
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
 
 ;; CC Mode
 (require 'cc-mode)
+(setq-default c-basic-offset 4)
 (setq c-default-style "linux")
 
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-rouge t)
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
-;; --------------------------------------------------------------------------------------
+;; hide minor modes from mode line
+(use-package diminish
+  :ensure t)
+
+(use-package smartparens
+  :ensure t
+  :diminish smartparens-mode
+  :config
+  (progn
+    (require 'smartparens-config)
+    (smartparens-global-mode 1)
+    (show-paren-mode t)))
+
+;; autocomplete
+(use-package company
+  :ensure t
+  :diminish company-mode
+  :config
+  (Add-hook 'after-init-hook #'global-company-mode))
+
+;; syntax checking 
+(use-package flycheck
+  :ensure t
+  :diminish flycheck-mode
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
+;; Yasnippit
+(use-package yasnippet
+  :defer 2
+  :config
+  (yas-global-mode 1))
+(use-package yasnippet-snippets
+  :defer)
+
+;; emacs server
+(require 'server)
+(if (not (server-running-p)) (server-start))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -146,9 +108,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" default))
+   '("e2c926ced58e48afc87f4415af9b7f7b58e62ec792659fcb626e8cba674d2065" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" default))
  '(package-selected-packages
-   '(zenburn-theme ido-completing-read+ iedit lsp-ivy lsp-ui latex-preview-pane powerline zoom lsp-python-ms dap-mode which-key magit go-mode exec-path-from-shell yasnippet-snippets yasnippet lsp-mode counsel ivy flycheck company smartparens)))
+   '(diminish zoom zenburn-theme yasnippet-snippets which-key use-package smartparens smart-mode-line-powerline-theme molokai-theme magit lsp-ui lsp-python-ms lsp-ivy latex-preview-pane iedit ido-completing-read+ go-mode flycheck exotica-theme exec-path-from-shell dap-mode counsel company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
